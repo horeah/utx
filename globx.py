fffd2df959938774529d74ce94cdda3abde0bfea
Horea Haitonic <horeah@gmail.com>
2010-06-16 18:43:06 +0300

    Added command line arguments for detailed+pretty printing, verbosity level, help.

--- globx.py
+++ globx.py
@@ -1,4 +1,6 @@
-import sys, os, fnmatch
+import sys, os, fnmatch, time
+
+special_entries = ['.', '..']
 
 def globx(directory, pattern):
 #    print [directory, pattern]
@@ -7,7 +9,6 @@ def globx(directory, pattern):
         
     (elem, _, rest) = pattern.partition('\\')
     files = os.listdir(directory)
-    results = []
     if elem == '**':
         for f in files:
             if os.path.isdir(directory + '\\' + f):
@@ -16,10 +17,35 @@ def globx(directory, pattern):
         for g in globx(directory, rest):
             yield g
     else:
-        for f in files:
+        for f in special_entries + files:
             if fnmatch.fnmatch(f, elem):
-                if rest == '':
+                if rest == '' and not f in special_entries:
                     yield f
                 if os.path.isdir(directory + '\\' + f):
                     for g in globx(directory + '\\' + f, rest):
                         yield f + '\\' + g
+
+def format_size(bytes, pretty = False):
+    if pretty:
+        if bytes < 1024:
+            return str(bytes)
+        elif bytes < 1024 * 1024:
+            return '%.1fK' % (bytes / 1024.0)
+        elif bytes < 1024 * 1024 * 1024:
+            return '%.1fM' % (bytes / (1024 * 1024.0))
+        else:
+            return '%.1fG' % (bytes / (1024 * 1024 * 1024.0))
+    else:
+        return str(bytes)
+
+def format_time(seconds, pretty = False):
+    if pretty:
+        if time.localtime(seconds).tm_year == time.localtime().tm_year:
+            format = '%b %d %I:%M'
+        else:
+            format = '%Y %b %d'
+        return time.strftime(format, time.localtime(seconds))
+    else:
+        return str(int(seconds))
+
+
--- lsx.py
+++ lsx.py
@@ -1,23 +1,64 @@
-import sys, globx
+import sys, globx, optparse, os
 
 
 def main():
-    if len(sys.argv) == 1:
+    # Define and parse command line otions 
+    parser = optparse.OptionParser(usage = 'lsx [options] <files>',
+                                   version = '10.1')
+    parser.add_option('-l', '--long', 
+                      action = 'store_true', dest = 'long_format', 
+                      default = False,
+                      help = 'display detailed file information')
+    parser.add_option('-p', '--pretty',
+                      action = 'store_true', dest = 'pretty_print',
+                      default = False,
+                      help = 'show human-readable file sizes')
+    parser.add_option('-v', '--verbose',
+                      action = 'store_true', dest = 'verbose',
+                      default = False,
+                      help = 'describe the operations being performed')
+    (options, args) = parser.parse_args()
+
+    if len(args) == 0:
         directory = '.'
         pattern = '**\*'
-    else:
-        sep_idx = sys.argv[1].find('\\\\')
+    elif len(args) == 1:
+        sep_idx = args[0].find('\\\\')
         if sep_idx >= 0:
-            directory = sys.argv[1][:sep_idx]
-            pattern = sys.argv[1][sep_idx + 2:]
+            directory = args[0][:sep_idx]
+            pattern = args[0][sep_idx + 2:]
         else:
             directory = '.'
-            pattern = sys.argv[1]
-    print 'Listing "' + pattern + '" based at "' + directory + '":'
+            pattern = args[0]
+
+    if options.verbose:
+        print '>> Listing "' + pattern + '" based at "' + directory + '":'
+
+    col_width = 12
+    col_spacing = 4
 
     results = globx.globx(directory, pattern)
     for elem in results:
-        print elem
+        line = elem
+        if options.long_format:
+            # Size
+            try:
+                size_str = globx.format_size(os.path.getsize(directory + '\\' + elem), options.pretty_print)
+            except os.error:
+                size_str = '??'
+            sys.stdout.write(' ' * (col_width - len(size_str)))
+            sys.stdout.write(size_str)
+            sys.stdout.write(' ' * col_spacing)
+            
+            # Modification time
+            try:
+                mtime_str = globx.format_time(os.path.getmtime(directory + '\\' + elem), options.pretty_print)
+            except os.error:
+                mtime_str = '??'
+            sys.stdout.write(' ' * (col_width - len(mtime_str)))
+            sys.stdout.write(mtime_str)
+            sys.stdout.write(' ' * col_spacing)
+        print line
 
 if __name__ == '__main__':
     main()
