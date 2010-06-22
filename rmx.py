28a3231f89129a015745a87c781613f83273a664
Horea Haitonic <horeah@gmail.com>
2010-06-22 11:09:00 +0300

    The interactive flag now supports interactivity levels
    The level argument (0..3) defines the granularity of the confirmation requirements

--- rmx.py
+++ rmx.py
@@ -14,38 +14,32 @@ def main():
     # Define and parse command line otions 
     parser = optparse.OptionParser(usage = 'rmx [options] <files>',
                                    version = '10.1')
-    parser.add_option('-i', '--interactive-cautious', 
-                      action = 'store_true', dest = 'interactive_cautious', 
-                      default = False,
-                      help = 'cautiously (y/N) confirm deletion for each file')
-    parser.add_option('-I', '--interactive-brave', 
-                      action = 'store_true', dest = 'interactive_brave', 
-                      default = False,
-                      help = 'bravely (Y/n) confirm deletion for each file')
+    parser.add_option('-i', '--interactive', 
+                      action = 'store', type = 'int', dest = 'interactive', 
+                      default = None,
+                      metavar = 'level',
+                      help = 'interactively confirm deletion (level=0..3)')
     parser.add_option('-v', '--verbose',
                       action = 'store_true', dest = 'verbose',
                       default = False,
                       help = 'describe the operations being performed')
     (options, args) = parser.parse_args()
 
-    # Arguments sanity check
-    if options.interactive_brave and options.interactive_cautious:
-        parser.error("options -i and -I are mutually exclusive")
-
-    # Interpret the <files> argument
+    # The files argument
     if len(args) == 0:
         parser.error('You must specify the files to delete')
-    else:
-        (directory, pattern) = globx.split_target(args[0])
+    (directory, pattern) = globx.split_target(args[0])
 
-    # Prepare the interactive mode (if specified)
-    if pattern.find('**') >= 0:
-        options.interactive_cautious = True
-    if options.interactive_brave:
-        default_input = 'Y'
-    elif options.interactive_cautious:
-        default_input = 'N'
-    options.interactive = options.interactive_cautious or options.interactive_brave
+    # The interactive level
+    if options.interactive == None:
+        if pattern.find('**') >= 0:
+            options.interactive = 3
+        else:
+            options.interactive = 0
+    if not options.interactive in range(0, 4):
+        parser.error('The interactive level has to be in [0..3]')
+
+
 
     if options.verbose:
         print '>> Deleting "' + pattern + '" based at "' + directory + '":'
@@ -53,20 +47,25 @@ def main():
     results = globx.globx(directory, pattern)
     abort = False
     confirm_initial = None
+    collected_results = []
     for elem in results:
-        if options.interactive:
+        if options.interactive == 0:
+            confirm = True
+        elif options.interactive == 1:
+            confirm = False
+        else:    # options.interactive > 1
             confirm = confirm_initial
             while confirm == None :
                 stdout.write('Delete "' + elem + '"? [')
-                if options.interactive_brave:
+                if options.interactive == 2:
                     stdout.write('Y/n')
-                else:   # options.interactive_cautious
+                else:   # options.interactive == 3
                     stdout.write('y/N')
                 stdout.write('/a(ll)/q(uit)]: ')
 
                 read = stdin.readline()
                 if len(read) == 1:
-                    confirm = options.interactive_brave
+                    confirm = options.interactive == 2
                 elif len(read) == 2:
                     if read[0].upper() == 'Y':
                         confirm = True
@@ -80,20 +79,56 @@ def main():
                         break
             if abort:
                 break
-        else:
-            confirm = True      # Confirmed by default in non-interactive
 
         if confirm:
-            full_name = directory + '\\' + elem
-            try:
-                if os.path.isdir(full_name):
-                    os.rmdir(full_name)
-                else:
-                    os.remove(full_name)
-                if options.verbose:
-                    stdout.write('>> Deleted "' + elem + '"\n')
-            except OSError, e:
-                stderr.write('  ' + e)
+            # Confirmed for this file, execute deletion
+            remove_file(directory, elem, options.verbose)
+        elif options.interactive == 1:
+            # In interactive == 1, just list the files and confirm at the end
+            collected_results.append(elem)
+            stdout.write(elem + '\n')
+    
+
+    if options.interactive == 1:
+        # Confirmation at the end
+        if len(collected_results) > 0:
+            confirm = None
+            while confirm == None :
+                stdout.write('Delete ' + str(len(collected_results)) + ' files? [y/N]: ')
+                read = stdin.readline()
+                if len(read) == 1:
+                    confirm = False
+                elif len(read) == 2:
+                    if read[0].upper() == 'Y':
+                        confirm = True
+                    elif read[0].upper() == 'N':
+                        confirm = False
+
+            if confirm:
+                for elem in collected_results:
+                    remove_file(directory, elem, options.verbose)
+        else:
+            stdout.write('No files to delete\n')
+
+                
+def remove_file(directory, name, verbose = False):
+    """
+    Try to delete a specified file or directory
+
+    Report any error; report what's being done if the verbose flag is 
+    True
+    """
+    full_name = directory + '\\' + name
+    try:
+        if os.path.isdir(full_name):
+            os.rmdir(full_name)
+        else:
+            os.remove(full_name)
+            if verbose:
+                stdout.write('>> Deleted "' + name + '"\n')
+    except OSError, e:
+        stderr.write('  ' + str(e) + '\n')
+    
 
 
 if __name__ == '__main__':
